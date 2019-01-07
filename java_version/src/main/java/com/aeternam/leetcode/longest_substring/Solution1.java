6ef25861e8f0bd3b197361e0f1a4dac84a531216
Li Beibei <libeibei@kelvins.io>
2019-01-07 16:50:33 +0800

    longest substring, problem 3

---
+++ java_version/src/main/java/com/aeternam/leetcode/longest_substring/Solution1.java
@@ -0,0 +1,34 @@
+/**
+ * Leetcode - longest_substring
+ */
+package com.aeternam.leetcode.longest_substring;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int len;
+        if (s == null || (len = s.length()) == 0) return 0;
+        int preP = 0, max = 0;
+        int[] hash = new int[128];
+        for (int i = 0; i < len; ++i) {
+            char c = s.charAt(i);
+            if (hash[c] > preP) {
+                preP = hash[c];
+            }
+            int l = i - preP + 1;
+            hash[c] = i + 1;
+            if (l > max) max = l;
+        }
+        return max;
+    }
+}
